59c664dd9c592b7f68f0a1972deda23b39cba599
aleksandramaria <ola.ksi@gmail.com>
2017-07-03 18:59:12 +0100

    second commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -3,5 +3,20 @@ package com.kodilla;
 /**
  * Created by ola on 03/07/2017.
  */
-public class Calculator {
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Calculator
+{
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
 }
+
+
